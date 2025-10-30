c175e112d86372b636571a5db02e46de9b862634
Fredi1574 <Fredi1574@gmail.com>
2025-10-31 00:24:05 +0200

    Changed the image card size

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -37,7 +37,7 @@ async function getProjects() {
 export default async function PortfolioPage() {
   const projects = await getProjects();
   return (
-    <div className="grid mb-6 mt-3 mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid mb-6 mt-3 mx-auto w-full max-w-5xl px-4 sm:px-6 lg:px-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {projects.map((project) => (
         <div key={project.slug}>
           <ProjectCard
